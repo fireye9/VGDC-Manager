bca345fd6a10039d237cc7375f345e3eeb00dd41
Anthony Ha <haa4@uci.edu>
2018-02-01 20:01:06 -0800

    Add: prototype problem/choice prompts

---
+++ Assets/Scripts/TimedPrompt.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimedPrompt : MonoBehaviour {
+
+    public GameObject promptPrefab;
+    GameObject newPrompt;
+
+    void Awake () {
+        Invoke("CreateNewPrompt", 2f);
+	}
+	
+    void CreateNewPrompt()
+    {
+        newPrompt = Instantiate(promptPrefab);
+        newPrompt.transform.parent = GameObject.Find("UICanvas").transform;
+        Button[] buttons = newPrompt.GetComponentsInChildren<Button>();
+        foreach (Button bt in buttons)
+        {
+            bt.onClick.AddListener(delegate { OnChoiceClick(); });
+        }
+
+    }
+    public void OnChoiceClick()
+    {
+        //Create an event for GameManager to listen to for gamestate effects
+        Destroy(newPrompt);
+    }
+}
