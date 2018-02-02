b0cb109873bc92107cc761286f3bfeb38850ab38
Anthony Ha <haa4@uci.edu>
2018-02-01 20:01:06 -0800

    Add: prototype problem/choice prompts

---
+++ Assets/Scripts/PromptPanel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PromptPanel : MonoBehaviour {
+    public TextAsset textFile;
+    public Text textObject;
+
+    private void Awake()
+    {
+        textObject = transform.Find("PromptText").GetComponent<Text>();
+        textObject.text = textFile.text;
+    }
+
+}
