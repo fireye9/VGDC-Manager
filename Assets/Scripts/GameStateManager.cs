861336419e36ac7a6e9f101007bf6398df9bd979
Anthony Ha <haa4@uci.edu>
2018-02-08 20:05:38 -0800

    Added: Calendar functions, prompt pop-ups at end of week

--- Assets/Scripts/GameStateManager.cs
+++ Assets/Scripts/GameStateManager.cs
@@ -14,44 +14,67 @@ public class GameStateManager : MonoBehaviour {
     float productivity = 20f;
     Slider productivityField;
 
+    int week = 1;
+    float day = 1;
+    Text calendarField;
+
     Canvas gameCanvas;
 
     private void Awake()
     {
-        gameCanvas = GameObject.Find("Canvas").GetComponent<Canvas>();
+        gameCanvas = GameObject.Find("UICanvas").GetComponent<Canvas>();
         membersField = gameCanvas.transform.Find("Members").GetComponent<Text>();
         satisfactionField = gameCanvas.transform.Find("Satisfaction").GetComponent<Text>();
         productivityField = gameCanvas.transform.Find("Productivity").GetComponent<Slider>();
-        
+        calendarField = gameCanvas.transform.Find("Calendar").GetComponent<Text>();
 
         membersField.text = members.ToString("F0");
         satisfactionField.text = satisfaction.ToString("F0");
         productivityField.value = productivity;
-    }
 
+
+    }
     public void SetSatisfaction(float newValue)
     {
         satisfaction = newValue;
         satisfactionField.text = satisfaction.ToString("F0") + "%";
     }
-
     public void SetMembers(float newValue)
     {
         members = newValue;
         membersField.text = members.ToString("F0");
     }
-
     public void SetProductivity(float newValue)
     {
         productivity = newValue;
         productivityField.value = productivity;
     }
+    public void SetDay(float newValue)
+    {
+        if (newValue > 7.0f)
+        {
+            Time.timeScale = 0;
+            PopUpPrompt();
+            week += 1;
+            day = 1f;
+        }
+        else
+            day = newValue;
+        calendarField.text = "Week " + week.ToString() + " Day " + day.ToString("F0");
+    }
+
+    void PopUpPrompt()
+    {
+        GetComponent<TimedPrompt>().CreateNewPrompt();
+    }
+
     private void FixedUpdate()
     {
         //Demo function calls that will only increment the game values over time
         SetSatisfaction(satisfaction + 1 * Time.deltaTime * 1.25f);
         SetMembers(members + 1 * Time.deltaTime * 0.2f);
         if (productivity < 100)
-            SetProductivity(productivity + 1 * Time.deltaTime * 2f);
+            SetProductivity(productivity + 1 * Time.deltaTime * 1.2f);
+        SetDay(day + Time.deltaTime * 0.5f);
     }
 }
--- Assets/Scripts/TimedPrompt.cs
+++ Assets/Scripts/TimedPrompt.cs
@@ -7,25 +7,24 @@ public class TimedPrompt : MonoBehaviour {
 
     public GameObject promptPrefab;
     GameObject newPrompt;
+    GameObject gameManager;
 
-    void Awake () {
-        Invoke("CreateNewPrompt", 2f);
-	}
-	
-    void CreateNewPrompt()
+    public void CreateNewPrompt()
     {
         newPrompt = Instantiate(promptPrefab);
-        newPrompt.transform.parent = GameObject.Find("UICanvas").transform;
+        newPrompt.transform.SetParent(GameObject.Find("UICanvas").transform,false); 
         Button[] buttons = newPrompt.GetComponentsInChildren<Button>();
         foreach (Button bt in buttons)
         {
             bt.onClick.AddListener(delegate { OnChoiceClick(); });
         }
+        //todo: be able to consolidate all relevant prompt situations and change the prompt's contents randomly
+        //look here: https://answers.unity.com/questions/711778/adding-prefabs-to-a-list-or-an-array-from-a-folder.html
 
     }
     public void OnChoiceClick()
     {
-        //Create an event for GameManager to listen to for gamestate effects
+        Time.timeScale = 1;
         Destroy(newPrompt);
     }
 }
