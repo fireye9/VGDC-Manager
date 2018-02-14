5f5b8e85f8b120d6d45d38e2dd2ba0c58898f046
Anthony Ha <haa4@uci.edu>
2018-02-13 20:44:04 -0800

    Implemented pulling choice buttons' text from a text file

--- Assets/Scripts/PromptPanel.cs
+++ Assets/Scripts/PromptPanel.cs
@@ -4,17 +4,31 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class PromptPanel : MonoBehaviour {
-    public TextAsset textFile;
+    public TextAsset textFilePrompts;
+    public TextAsset textFileChoices;
     public Text textObject;
     private string[] prompts;
-
+    private string[][] choices;
 
     private void Awake()
     {
-        prompts = textFile.text.Split('\n');
+        
+        prompts = textFilePrompts.text.Split('\n');
+        choices = new string[prompts.Length][];
+        string[] choiceLines = textFileChoices.text.Split('\n');
+        for (int i = 0; i < choiceLines.Length; i++)
+        {
+            choices[i] = choiceLines[i].Split(',');
+        }
         textObject = transform.Find("PromptText").GetComponent<Text>();
         int rnd = Random.Range(0, prompts.Length);
         textObject.text = prompts[rnd];
+        for (int i = 0; i < 3; i++)
+        {
+            Transform child;
+            child = transform.Find("Choices").GetChild(i);
+            child.Find("Text").GetComponent<Text>().text = choices[rnd][i];
+        }
     }
 
 }
