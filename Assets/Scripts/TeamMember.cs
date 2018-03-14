7360da4299ccc2193120b6718703b7e934eae06f
nuclearMonkeys <33884355+nuclearMonkeys@users.noreply.github.com>
2018-03-13 20:48:32 -0700

    Scripts Changes

---
+++ Assets/Scripts/TeamMember.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeamMember : MonoBehaviour
+{
+    // all these objects will be randomly assigned publicly with the random generator(s)
+    // Yes, I know that name is not used yet...
+
+    public string name;
+    public string role;
+    public Team team;
+    public bool isLead = false;
+}
