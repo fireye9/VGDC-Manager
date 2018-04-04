4fd2062689eff40fba38ffe92499b5b4d5833674
Anthony Ha <haa4@uci.edu>
2018-04-03 17:45:37 -0700

    Title Screen final

--- Assets/Scripts/TitleScreen.cs
+++ Assets/Scripts/TitleScreen.cs
@@ -5,8 +5,36 @@ using UnityEngine.SceneManagement;
 
 public class TitleScreen : MonoBehaviour {
 
+    public GameObject mainMenu;
+    public GameObject introText;
+    public GameObject credits;
+
+    public AudioClip buttonsound;
+
 	public void OnStartClick()
     {
+        PlayButtonSound();
+        mainMenu.SetActive(false);
+        introText.SetActive(true);
+    }
+    public void OnIntroCloseClick()
+    {
+        PlayButtonSound();
         SceneManager.LoadScene(1);
+    }
+    public void OnCreditsClick()
+    {
+        PlayButtonSound();
+        mainMenu.SetActive(!mainMenu.activeSelf);
+        credits.SetActive(!mainMenu.activeSelf);
+    }
+    public void OnExitClick()
+    {
+        PlayButtonSound();
+        Application.Quit();
+    }
+    void PlayButtonSound()
+    {
+        GetComponent<AudioSource>().PlayOneShot(buttonsound);
     }
 }
